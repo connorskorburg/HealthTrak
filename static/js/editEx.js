ae7cb4f9d6e772906e5a93ae0f579373319a18bd
Connor <connorskorburg@gmail.com>
2020-08-21 10:02:02 -0500

    added js file to calc minutes and hours

---
+++ static/js/editEx.js
@@ -0,0 +1,10 @@
+let duration = document.getElementById('duration').value;
+
+function calcHourMin(duration){
+    let durList = []
+    let hour = duration / 60;
+    if(hour < 1){
+        hour = 0;
+    }
+    let minutes = 
+}
