a4eb9ba73083a4b681a7a5b644550a7bcea15246
Connor <connorskorburg@gmail.com>
2020-09-08 18:02:27 -0500

    added toggler to mobile nav

--- static/js/toggler.js
+++ static/js/toggler.js
@@ -1,3 +1,8 @@
+const mobileNav = document.getElementById('home-nav-mobile');
 document.getElementById('hamburger').addEventListener('click', () => {
-  
+  if(mobileNav.style.display === '' || mobileNav.style.display === 'none') {
+    mobileNav.style.display = 'block';
+  } else if (mobileNav.style.display === 'block') {
+    mobileNav.style.display = 'none';
+  }
 });
