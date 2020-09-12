bf766edc4dff3b7af537e7a39ca9bdfa3137a9ee
Connor <connorskorburg@gmail.com>
2020-09-11 21:58:20 -0500

    added popup form without toggle

--- static/js/foodToggle.js
+++ static/js/foodToggle.js
@@ -3,4 +3,15 @@ console.log(results);
 
 let parsedResults = JSON.parse(results.textContent);
 
+
+let myBtns = document.querySelectorAll('.res-btn');
+// document.querySelector('.res-btn').addEventListener('onclick', () => {
+  // console.log("hello");
+// })
+
+myBtns.forEach(btn => {
+  console.log(btn);
+});
+
+console.log(myBtns);
 console.log(parsedResults);
