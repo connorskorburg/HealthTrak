ae8b4c0848870a561e17a71ff792a6887912aa3f
Connor <connorskorburg@gmail.com>
2020-09-19 20:08:28 -0500

    updated config file

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from flask import Flask 
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-from secret import secret_key, pw
+from secret import secret_key, uri
 
 app = Flask(__name__)
 
 app.secret_key = secret_key
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{pw}@localhost/health_tracker'
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db = SQLAlchemy(app)
