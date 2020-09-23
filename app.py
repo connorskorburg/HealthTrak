83c08cfa622e57bbf1614651d5c4ab52c143d9cb
Connor <connorskorburg@gmail.com>
2020-09-23 12:42:56 -0500

    updated config

--- app.py
+++ app.py
@@ -3,6 +3,5 @@ from config import app, db
 from models import * 
 import routes 
 
-
 if __name__ == "__main__": 
     app.run(debug=True)
--- secret.py
+++ secret.py
@@ -1,3 +1,3 @@
 key = 'OvjuhIReozO7qv9QdXrqsUhoal6jkvXmCpRW795t'
 secret_key = 'health_app_practice1234qwertzgadfsasd'
-uri = 'mysql+pymysql://root:root@mysql:3306/health_trak_final_practice'
+uri = 'mysql+pymysql://root:root@localhost/health_tracker'
